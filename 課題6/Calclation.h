7f896b4c28546989ae5014eba7edb4413e98ddbd
rikusato <230554@jc-21.jp>
2024-06-18 10:25:21 +0900

    課題6完成

---
+++ 課題6/Calclation.h
@@ -0,0 +1,10 @@
+#pragma once
+class Calclation{
+	float Avalue;
+	float Bvalue;
+
+public:
+	void SetA(float i);
+	void SetB(float p);
+	void Disp();
+};
---
+++ 課題6/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"Calclation.h"
+
+int main() {
+	float a = 42.195f;
+	float b = 3.14f;
+
+	Calclation x;
+
+	x.SetA(a);
+	x.SetB(b);
+	x.Disp();
+}
