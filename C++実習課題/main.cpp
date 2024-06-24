77e4b20d0e37c72530ce03c5df0dc2d4804c657e
rikusato <230554@jc-21.jp>
2024-06-24 11:20:59 +0900

    kadai12

--- C++実習課題/main.cpp
+++ C++実習課題/main.cpp
@@ -1,10 +1,15 @@
-#include"Sample.h"
-
-
-int main() {
-	SampleClass a;
-
-	a.Input();
-	a.Plus();
-	a.Disp();
+#include "Sample.h"
+//SampleClass クラスのポインタを用意
+SampleClass* a;
+//メイン関数
+int main()
+{
+	//クラスのインスタンス（実態）を作る
+	a = new SampleClass;
+	//３つのメンバ関数を呼び出す
+	a->Input();
+	a->Plus();
+	a->Disp();
+	//使い終わったインスタンスを削除
+	delete a;
 }
